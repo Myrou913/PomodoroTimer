f62a07d2fc20ed881765424a1c1304bd5a4763f4
Mariem Sebai <mariemsebai913@gmail.com>
2025-08-25 22:36:26 +0100

    success

--- Pomodoro-timer.js
+++ Pomodoro-timer.js
@@ -37,3 +37,48 @@ listContainer.addEventListener('click', (e) => {
     e.target.parentElement.remove();
   }
 });
+
+document.addEventListener('click', (e) => {
+  if (toDoList.style.display === 'block' && !listContainer.contains(e.target) && e.target !== toDoListIcon) {
+    toDoList.style.display = 'none';
+  }
+});
+
+//settings
+
+const settingsIcon = document.getElementById('main__settings-icon');
+const settings = document.getElementById('settings');
+const settingsContainer = document.getElementById('settings__container');
+
+settingsIcon.addEventListener('click', () => {
+  if (settings.style.display === 'block') {
+    settings.style.display = 'none';
+  }else {
+    settings.style.display = 'block';
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (settings.style.display === 'block' && !settingsContainer.contains(e.target) && e.target !== settingsIcon) {
+    settings.style.display = 'none';
+  }
+});
+
+const themes = document.getElementById('themes');
+const timer = document.getElementById('timer');
+const settingsElements1 = document.getElementById('settings__elements1');
+const settingsElements2 = document.getElementById('settings__elements2');
+
+themes.addEventListener('click', () => {
+  if (getComputedStyle(settingsElements2).display === 'none') {
+    settingsElements2.style.display = 'block';
+    settingsElements1.style.display = 'none';
+  }
+});
+
+timer.addEventListener('click', () => {
+  if (getComputedStyle(settingsElements1).display === 'none') {
+    settingsElements1.style.display = 'flex';
+    settingsElements2.style.display = 'none'; 
+  }
+});
