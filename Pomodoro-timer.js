bb0f04ff02206386984f2a6fede670adddc564bd
Mariem Sebai <mariemsebai913@gmail.com>
2025-08-26 12:40:36 +0100

    success

--- Pomodoro-timer.js
+++ Pomodoro-timer.js
@@ -38,12 +38,6 @@ listContainer.addEventListener('click', (e) => {
   }
 });
 
-document.addEventListener('click', (e) => {
-  if (toDoList.style.display === 'block' && !listContainer.contains(e.target) && e.target !== toDoListIcon) {
-    toDoList.style.display = 'none';
-  }
-});
-
 //settings
 
 const settingsIcon = document.getElementById('main__settings-icon');
@@ -81,4 +75,132 @@ timer.addEventListener('click', () => {
     settingsElements1.style.display = 'flex';
     settingsElements2.style.display = 'none'; 
   }
+});
+
+//change pink wallpaper
+
+const pink1 = document.getElementById('pink__theme1');
+const pink2 = document.getElementById('pink__theme2');
+const pink3 = document.getElementById('pink__theme3');
+
+pink1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/pink1.jpg')"
+});
+pink2.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/pink2.jpg')"
+});
+pink3.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/pink3.jpg')"
+});
+
+//change blue wallpaper
+
+const blue1 = document.getElementById('blue1__theme');
+const blue2 = document.getElementById('blue2__theme');
+const blue3 = document.getElementById('blue3__theme');
+const namE = document.getElementById('header__name');
+const nameSpan = document.getElementById('header__span');
+const focusButton = document.getElementById('focus__button');
+const breakButton = document.getElementById('break__button');
+const mainTimer = document.getElementById('main__timer');
+const startButton = document.getElementById('start__button');
+const stopButton = document.getElementById('stop__button');
+const resetButton = document.getElementById('reset__button');
+
+blue1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/blue1.jpg')" ;
+  toDoListIcon.style.color = "#7496c0";
+  namE.style.color = "#7496c0";
+  nameSpan.style.color = "#274579";
+  focusButton.style.color = "#274579";
+  breakButton.style.backgroundColor = "#274579";
+  mainTimer.style.webkitTextStroke = "2px #274579";
+  mainTimer.style.textShadow = "10px 6px 0 #7496c0";
+  startButton.style.backgroundColor = "#274579";
+  stopButton.style.backgroundColor = "#274579";
+  resetButton.style.backgroundColor = "#274579";
+  settingsIcon.style.color = "#274579";
+  addButton.style.backgroundColor = '#274579';
+});
+blue2.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/blue2.jpg')" ;
+  toDoListIcon.style.color = "#7496c0";
+  namE.style.color = "#7496c0";
+  nameSpan.style.color = "#274579";
+  focusButton.style.color = "#274579";
+  breakButton.style.backgroundColor = "#274579";
+  mainTimer.style.webkitTextStroke = "2px #274579";
+  mainTimer.style.textShadow = "10px 6px 0 #7496c0";
+  startButton.style.backgroundColor = "#274579";
+  stopButton.style.backgroundColor = "#274579";
+  resetButton.style.backgroundColor = "#274579";
+  settingsIcon.style.color = "#274579";
+  addButton.style.backgroundColor = '#274579';
+});
+blue3.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/blue3.jpg')" ;
+  toDoListIcon.style.color = "#7496c0";
+  namE.style.color = "#7496c0";
+  nameSpan.style.color = "#274579";
+  focusButton.style.color = "#274579";
+  breakButton.style.backgroundColor = "#274579";
+  mainTimer.style.webkitTextStroke = "2px #274579";
+  mainTimer.style.textShadow = "10px 6px 0 #7496c0";
+  startButton.style.backgroundColor = "#274579";
+  stopButton.style.backgroundColor = "#274579";
+  resetButton.style.backgroundColor = "#274579";
+  settingsIcon.style.color = "#274579";
+  addButton.style.backgroundColor = '#274579';
+});
+
+//change green wallpaper
+
+const green1 = document.getElementById('green1__theme');
+const green2 = document.getElementById('green2__theme');
+const green3 = document.getElementById('green3__theme');
+
+green1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/kawaii1.jpg')" ;
+  toDoListIcon.style.color = "#60b442";
+  namE.style.color = "#60b442";
+  nameSpan.style.color = "#24351e";
+  focusButton.style.color = "#24351e";
+  breakButton.style.backgroundColor = "#24351e";
+  mainTimer.style.webkitTextStroke = "2px #24351e";
+  mainTimer.style.textShadow = "10px 6px 0 #60b442";
+  startButton.style.backgroundColor = "#24351e";
+  stopButton.style.backgroundColor = "#24351e";
+  resetButton.style.backgroundColor = "#24351e";
+  settingsIcon.style.color = "#24351e";
+  addButton.style.backgroundColor = '#24351e';
+});
+green2.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/kawaii2.jpg')" ;
+  toDoListIcon.style.color = "#60b442";
+  namE.style.color = "#60b442";
+  nameSpan.style.color = "#24351e";
+  focusButton.style.color = "#24351e";
+  breakButton.style.backgroundColor = "#24351e";
+  mainTimer.style.webkitTextStroke = "2px #24351e";
+  mainTimer.style.textShadow = "10px 6px 0 #60b442";
+  startButton.style.backgroundColor = "#24351e";
+  stopButton.style.backgroundColor = "#24351e";
+  resetButton.style.backgroundColor = "#24351e";
+  settingsIcon.style.color = "#24351e";
+  addButton.style.backgroundColor = '#24351e';
+});
+green3.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../images/kawaii3.jpg')" ;
+  toDoListIcon.style.color = "#60b442";
+  namE.style.color = "#60b442";
+  nameSpan.style.color = "#24351e";
+  focusButton.style.color = "#24351e";
+  breakButton.style.backgroundColor = "#24351e";
+  mainTimer.style.webkitTextStroke = "2px #24351e";
+  mainTimer.style.textShadow = "10px 6px 0 #60b442";
+  startButton.style.backgroundColor = "#24351e";
+  stopButton.style.backgroundColor = "#24351e";
+  resetButton.style.backgroundColor = "#24351e";
+  settingsIcon.style.color = "#24351e";
+  addButton.style.backgroundColor = '#24351e';
 });
